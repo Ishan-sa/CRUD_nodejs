aca71d8996b3e831db1fe7bcb6651ba61ba4c781
Ishan-sa <ishansach19@gmail.com>
2022-12-12 02:00:40 -0800

    views created

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-    res.send('All Users');
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+})
+
+router.get('/add', (req, res) => {
+    res.render('addUser', { title: 'Add User' });
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
+require('dotenv').config({ path: './.env.local' });
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 
+
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+mongoose.set('strictQuery', true);
 /* MIDDLEWARE */
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({
     secret: 'secret',
@@ -19,14 +20,14 @@ app.use((req, res, next) => {
     res.locals.message = req.session.message;
     delete req.session.message;
     next();
-    });
+});
 app.use('', require('./routes/routes')); // route middleware
 
 /* SET TEMPLATE ENGINE */
 app.set('view engine', 'ejs');
 
 /* db connection */
-mongoose.connect(process.env.DB_URI, {useNewURLParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URI, { useNewURLParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
